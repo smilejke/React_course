e9d815e30d3ebae5b81ed29a0d15a2b65eef96f8
smilejke <smilejke1@mail.ru>
2020-01-25 00:43:35 +0300

    counter homework volume two. Made basic values and made render function independent from props

--- button_counter/index.js
+++ button_counter/index.js
@@ -1,33 +1,45 @@
 class Counter {
   constructor(hash) {
-    this.initial = hash.initial;
-    this.decrStep = hash.decrStep;
-    this.incrStep = hash.incrStep;
-    this.minRange = hash.minRange;
-    this.maxRange = hash.maxRange;
+    this.initial = hash.initial ? hash.initial : 0;
+    this.decrStep = hash.step.decrStep ? hash.step.decrStep : 1;
+    this.incrStep = hash.step.incrStep ? hash.step.incrStep : 1;
+    this.minRange = hash.range.minRange;
+    this.maxRange = hash.range.maxRange;
   }
 }
 
 const counter1 = new Counter({
   initial: 0,
-  decrStep: 1,
-  incrStep: 1,
-  minRange: 0,
-  maxRange: 10
+  step: {
+    decrStep: 1,
+    incrStep: 1
+  },
+  range: {
+    minRange: 0,
+    maxRange: 10
+  }
 });
 const counter5 = new Counter({
-  initial: 0,
-  decrStep: 5,
-  incrStep: 5,
-  minRange: 0,
-  maxRange: 25
+  initial: 10,
+  step: {
+    decrStep: 5,
+    incrStep: 5
+  },
+  range: {
+    minRange: -25,
+    maxRange: 25
+  }
 });
 const counter10 = new Counter({
   initial: 0,
-  decrStep: 10,
-  incrStep: 10,
-  minRange: -50,
-  maxRange: 100
+  step: {
+    decrStep: 10,
+    incrStep: 10
+  },
+  range: {
+    minRange: -50,
+    maxRange: 100
+  }
 });
 
 const renderOneCoutnerRow = counter => {
@@ -37,39 +49,45 @@ const renderOneCoutnerRow = counter => {
 
   const decrButton = document.createElement("input");
   decrButton.type = "button";
-  decrButton.value = `minus ${counter.decrStep}`;
+  decrButton.id = "decr";
+  decrButton.value = counter ? `minus ${counter.incrStep}` : "minus";
   decrButton.addEventListener("click", () => {
-    if (
-      `${counter.initial - counter.decrStep}` < counter.minRange ||
-      `${counter.initial - counter.decrStep}` > counter.maxRange
-    ) {
-      console.log("out of range");
-      return null;
-    }
-    output.innerText = parseInt(`${(counter.initial -= counter.decrStep)}`);
+    if (counter) {
+      if (
+        `${counter.initial - counter.decrStep}` < counter.minRange ||
+        `${counter.initial - counter.decrStep}` > counter.maxRange
+      ) {
+        console.log("out of range");
+        return null;
+      }
+      output.innerText = parseInt(`${(counter.initial -= counter.decrStep)}`);
+    } else output.innerText--;
   });
   div.appendChild(decrButton);
 
   const output = document.createElement("span");
   output.className = "counter";
-  output.innerText = `${counter.initial}`;
+  output.innerText = counter ? `${counter.initial}` : "0";
   div.appendChild(output);
 
   const incrButton = document.createElement("input");
   incrButton.type = type = "button";
-  incrButton.value = `plus ${counter.incrStep}`;
+  incrButton.value = counter ? `plus ${counter.incrStep}` : "plus";
   incrButton.addEventListener("click", () => {
-    if (
-      `${counter.initial + counter.incrStep}` < counter.minRange ||
-      `${counter.initial + counter.incrStep}` > counter.maxRange
-    ) {
-      console.log("out of range");
-      return null;
-    }
-    output.innerText = parseInt(`${(counter.initial += counter.incrStep)}`);
+    if (counter) {
+      if (
+        `${counter.initial + counter.incrStep}` < counter.minRange ||
+        `${counter.initial + counter.incrStep}` > counter.maxRange
+      ) {
+        console.log("out of range");
+        return null;
+      }
+      output.innerText = parseInt(`${(counter.initial += counter.incrStep)}`);
+    } else output.innerText++;
   });
   div.appendChild(incrButton);
 };
 renderOneCoutnerRow(counter1);
 renderOneCoutnerRow(counter5);
-renderOneCoutnerRow(counter10);
+// renderOneCoutnerRow(counter10);
+renderOneCoutnerRow();
