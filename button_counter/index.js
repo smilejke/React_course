ee45dcb6b723d9688fdb58c167cb81be97bb815d
smilejke <smilejke1@mail.ru>
2020-01-26 02:17:07 +0300

    reworked all work into class because could not split logic from production and added total values counter as task 3

--- button_counter/index.js
+++ button_counter/index.js
@@ -1,93 +1,105 @@
 class Counter {
   constructor(hash) {
     this.initial = hash.initial ? hash.initial : 0;
-    this.decrStep = hash.step.decrStep ? hash.step.decrStep : 1;
-    this.incrStep = hash.step.incrStep ? hash.step.incrStep : 1;
-    this.minRange = hash.range.minRange;
-    this.maxRange = hash.range.maxRange;
+    this.incrStep = hash.incrStep ? hash.incrStep : 1;
+    this.decrStep = hash.decrStep ? hash.decrStep : 1;
+    this.minRange = hash.minRange === undefined ? -Infinity : hash.minRange;
+    this.maxRange = hash.maxRange === undefined ? Infinity : hash.maxRange;
+    this.counter = this.initial;
+  }
+
+  decrement(event) {
+    const outputField = event.target.parentElement.querySelector(
+      ".output-field"
+    );
+
+    if (this.counter - this.decrStep >= this.minRange) {
+      this.counter = this.counter - this.decrStep;
+      outputField.innerText = this.counter;
+      this.getSum();
+    }
+  }
+
+  increment(event) {
+    const outputField = event.target.parentElement.querySelector(
+      ".output-field"
+    );
+
+    if (this.counter + this.incrStep <= this.maxRange) {
+      this.counter = this.counter + this.incrStep;
+      outputField.innerText = this.counter;
+      this.getSum();
+    }
+  }
+
+  getSum() {
+    let sum = 0;
+    let outputField = document.querySelectorAll(".output-field");
+    let totalText = document.getElementById("total");
+
+    for (let index = 0; index < outputField.length; index += 1) {
+      sum += parseInt(outputField[index].innerText);
+    }
+    totalText.innerText = `Total is ${sum}`;
+  }
+
+  render() {
+    if (marker) {
+      const total = document.createElement("p");
+      total.id = "total";
+      document.body.appendChild(total);
+      marker = false;
+    }
+
+    const decrButton = document.createElement("button");
+    decrButton.innerText = "minus";
+    decrButton.addEventListener("click", this.decrement.bind(this));
+
+    const output = document.createElement("span");
+    output.className = "output-field";
+    output.append(this.counter);
+
+    const incrButton = document.createElement("button");
+    incrButton.innerText = "plus";
+    incrButton.addEventListener("click", this.increment.bind(this));
+
+    const div = document.createElement("div");
+    div.className = "button-row";
+    div.append(decrButton, output, incrButton);
+    document.body.appendChild(div);
+
+    this.getSum();
+    return div;
   }
 }
 
 const counter1 = new Counter({
   initial: 0,
-  step: {
-    decrStep: 1,
-    incrStep: 1
-  },
-  range: {
-    minRange: 0,
-    maxRange: 10
-  }
+  decrStep: 1,
+  incrStep: 1,
+  minRange: 0,
+  maxRange: 10
 });
+
 const counter5 = new Counter({
   initial: 10,
-  step: {
-    decrStep: 5,
-    incrStep: 5
-  },
-  range: {
-    minRange: -25,
-    maxRange: 25
-  }
+  decrStep: 5,
+  incrStep: 5,
+  minRange: -25,
+  maxRange: 25
 });
+
 const counter10 = new Counter({
-  initial: 0,
-  step: {
-    decrStep: 10,
-    incrStep: 10
-  },
-  range: {
-    minRange: -50,
-    maxRange: 100
-  }
+  initial: 50,
+  decrStep: 10,
+  incrStep: 10,
+  minRange: -20,
+  maxRange: 100
 });
+// const counter10 = new Counter();
 
-const renderOneCoutnerRow = counter => {
-  const div = document.createElement("div");
-  div.className = "button-row";
-  document.body.appendChild(div);
+let marker = true;
 
-  const decrButton = document.createElement("input");
-  decrButton.type = "button";
-  decrButton.id = "decr";
-  decrButton.value = counter ? `minus ${counter.incrStep}` : "minus";
-  decrButton.addEventListener("click", () => {
-    if (counter) {
-      if (
-        `${counter.initial - counter.decrStep}` < counter.minRange ||
-        `${counter.initial - counter.decrStep}` > counter.maxRange
-      ) {
-        console.log("out of range");
-        return null;
-      }
-      output.innerText = parseInt(`${(counter.initial -= counter.decrStep)}`);
-    } else output.innerText--;
-  });
-  div.appendChild(decrButton);
-
-  const output = document.createElement("span");
-  output.className = "counter";
-  output.innerText = counter ? `${counter.initial}` : "0";
-  div.appendChild(output);
-
-  const incrButton = document.createElement("input");
-  incrButton.type = type = "button";
-  incrButton.value = counter ? `plus ${counter.incrStep}` : "plus";
-  incrButton.addEventListener("click", () => {
-    if (counter) {
-      if (
-        `${counter.initial + counter.incrStep}` < counter.minRange ||
-        `${counter.initial + counter.incrStep}` > counter.maxRange
-      ) {
-        console.log("out of range");
-        return null;
-      }
-      output.innerText = parseInt(`${(counter.initial += counter.incrStep)}`);
-    } else output.innerText++;
-  });
-  div.appendChild(incrButton);
-};
-renderOneCoutnerRow(counter1);
-renderOneCoutnerRow(counter5);
-// renderOneCoutnerRow(counter10);
-renderOneCoutnerRow();
+counter1.render();
+counter5.render();
+counter10.render();
